0923d5b974c8dd16fcc776b82cdb64ceab393078
GIGANico <ni.gutierrezo@duocuc.cl>
2024-04-28 22:53:25 -0400

    Vistas admin y routes hacia ellas

---
+++ src/app/admin-todas-reservas/admin-todas-reservas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-todas-reservas',
+  templateUrl: './admin-todas-reservas.component.html',
+  styleUrl: './admin-todas-reservas.component.css'
+})
+export class AdminTodasReservasComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,19 +9,33 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import {MaterialModule} from './modules/material/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AdminTodasReservasComponent } from './admin-todas-reservas/admin-todas-reservas.component';
+import { AdminTodosEmpleadosComponent } from './admin-todos-empleados/admin-todos-empleados.component'
+
+
+const routes: Routes = [
+  { path: 'admin_reservas', component: AdminTodasReservasComponent },
+  { path: 'admin_empleados', component: AdminTodosEmpleadosComponent },
+  { path: '', component: InicioComponent, pathMatch: 'full' }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent,
+    AdminTodasReservasComponent,
+    AdminTodosEmpleadosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     provideAnimationsAsync()
